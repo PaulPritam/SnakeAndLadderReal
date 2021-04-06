5aaebc22c9808fa40e5b0ecd5307974c58298faf
unknown <pritampaul9798@gmail.com>
2021-04-06 11:53:43 +0530

    added program for checker

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -4,6 +4,7 @@ import java.util.*;
 
 public class SnakeAndLadder {
 
+	
 	public static void main(String[] args) {
 		System.out.println("welcome to snake ladder game ");	
 		int pos=0;
@@ -11,6 +12,22 @@ public class SnakeAndLadder {
 		Random a = new Random();
 		int die = a.nextInt(6) + 1;
 		System.out.println("die rolled with a value of " +die);
+		int checker = a.nextInt(3);
+		System.out.println(checker);
+		if (checker == 0) {
+			System.out.println("the player remained in the same position ");
+		}
+		else if (checker == 1) 
+		{
+			pos = die;
+			System.out.println("the player moved to " +pos);
+		}
+		else
+		{
+			pos = pos - die;
+			System.out.println(" the player moved to " +pos);
+		}
+			
 	}
 
 }
