0050f341b499736ba0f21747591a431114102879
unknown <pritampaul9798@gmail.com>
2021-04-06 11:50:27 +0530

    added program to roll the die

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -8,7 +8,9 @@ public class SnakeAndLadder {
 		System.out.println("welcome to snake ladder game ");	
 		int pos=0;
 		System.out.println("initial position "+pos);
-		
+		Random a = new Random();
+		int die = a.nextInt(6) + 1;
+		System.out.println("die rolled with a value of " +die);
 	}
 
 }
