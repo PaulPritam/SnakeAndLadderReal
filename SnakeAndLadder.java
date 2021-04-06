230cf811388e64b2b09ee7354b7aa67f53a5b84b
unknown <pritampaul9798@gmail.com>
2021-04-06 11:48:17 +0530

    added welcome and programe to print initial position

---
+++ SnakeAndLadder.java
@@ -0,0 +1,14 @@
+package snake;
+
+import java.util.*;
+
+public class SnakeAndLadder {
+
+	public static void main(String[] args) {
+		System.out.println("welcome to snake ladder game ");	
+		int pos=0;
+		System.out.println("initial position "+pos);
+		
+	}
+
+}
