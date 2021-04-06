3935581e5261f198a64b2ed18d652484c7fece10
unknown <pritampaul9798@gmail.com>
2021-04-06 11:58:12 +0530

    added program for second player

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -7,14 +7,26 @@ public class SnakeAndLadder {
 	
 	public static void main(String[] args) {
 		System.out.println("welcome to snake ladder game ");
+		int p1 = 0;
+		int p2 = 0;
+		for (int i =0; i <=1; i++)
+		{
 		for (int pos=0; pos<=100;)
 		{
 			if (pos < 0 ) {
 				pos = 0;
 			}
-		System.out.println("initial position "+pos);
+		System.out.println(" position "+pos);
 		Random a = new Random();
 		int die = a.nextInt(6) + 1;
+		if (i == 0) {
+		p1 = p1++;
+		}
+		else
+		{
+		p2 = p2++;
+		}
+		
 		System.out.println("die rolled with a value of " +die);
 		int checker = a.nextInt(3);
 		System.out.println(checker);
@@ -34,18 +46,4 @@ public class SnakeAndLadder {
 				System.out.println("the player moved to " +pos);	
 			}
 			
-		
-		}
-		else 
-		{
-			pos = pos - die ;
-		System.out.println("got snaked " +pos);
-		}	
-		
-		if (pos == 100) {
-			break;
-		}
-	}
-	}	
-
 	}
